c6fafe914ff3dd5c3f79a15d61be204ec12fb31e
trenton michael cox <134955908+coxtrent@users.noreply.github.com>
2025-07-05 20:08:23 -0500

    damnit

--- src/components/TutorialSection.tsx
+++ src/components/TutorialSection.tsx
@@ -16,6 +16,7 @@ export type ChecklistItem = {
   text: string;
   code?: string; // Optional code block for this step
   downloads?: { href: string; label: string;  }[]; // Optional links for this step
+  links?: { href: string; label: string; }[]; // Optional links for this step
 };
 
 export type TutorialSectionProps = {
