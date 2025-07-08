fdf67d84e7caec7cb698e5a21d7195c432ac079d
trenton michael cox <134955908+coxtrent@users.noreply.github.com>
2025-07-08 13:45:03 -0500

    Add steps to tutorials.

--- src/app/endless-runner/score/page.tsx
+++ src/app/endless-runner/score/page.tsx
@@ -6,8 +6,41 @@
  
  const checklist: ChecklistItem[] = [
    {
-     text: "Follow along with the video to add a score system.",
-   }
+     text: "In order to use text in your game, add this line to the top of your PlayerController script, underneath the \"using UnityEngine;\" line.",
+     code: `using TMPro;`,
+   },
+   {
+    text: "Add this code underneath the line that says \"private float startZ;\"",
+    code: `private float startZ;
+    public float score = 0f;
+    public TMP_Text scoreText;`
+   },
+   {
+    text: "Add this code as the last line of the void Start() function in the PlayerController script, under \"rb = GetComponent<Rigidbody>();\"",
+    code: `        startZ = transform.position.z;`
+   },
+   {
+    text: "Add this code as the last lines of void Update(), under \"transform.position = pos;\".",
+    code: `        score = transform.position.z - startZ;
+        if (scoreText != null)
+            scoreText.text = ((int)score).ToString("D6");`
+   },
+   {
+   text: "OPTIONAL: You can send messages to the Console tab in the lower right of Unity with Debug.Log(\"Your message goes in here\");. Add that line at the end of the void Update() function to see your score in the Console tab.",
+   code: `        Debug.Log($"Score: {score}"); `
+   },
+   {
+    text: "Go back to the Unity window. Control+Click on the Canvas, go to UI, and create a new Text - TextMeshPro object. Name it \"ScoreText\".",
+   },
+   {
+    text: "Move the ScoreText object to whatever corner of the canvas you'd like. Remember that this canvas is essentially gonna be the screen while the game is running."
+   },
+   {
+    text: "Select Player in the Hierarchy. Drag and drop the ScoreText object from the Hierarchy into the Score Text field of the PlayerController script in the Inspector tab.",
+   },
+   {
+    text: "For some reason, this did not always work for me. You might have to refresh the PlayerController script by removing it and adding it back to the Player object in the Hierarchy and dragging the ScoreText back in.",
+   },
  ];
  
  export default function ScorePage() {
@@ -26,3 +59,5 @@
      </main>
    );
  }
+
+ 
--- src/app/endless-runner/terrain-recycling/page.tsx
+++ src/app/endless-runner/terrain-recycling/page.tsx
@@ -6,8 +6,63 @@
  
  const checklist: ChecklistItem[] = [
    {
-     text: "Follow along with the video to make the world endless.",
+     text: "Create a new folder in the Assets folder called \"Prefabs\".",
+   },
+   {
+    text: "Drag and drop your Ground object from the Hierarchy into the Prefabs folder. This creates a reusable template that the scripts we're about to make will need.",
+   },
+   {
+    text: "Create a new script in the Scripts folder called \"TerrainManager.cs\".",
+   },
+   {
+     text: "Add this code to the top of the TerrainManager script underneath \"using UnityEngine;\"",
+     code: `using System.Collections.Generic;`,
+   },
+   {
+    text: "Add this code to the terrainManager script directly above void Start(), underneath the line with \"public class TerrainManager : MonoBehaviour\".  Make sure the number in \"public float segmentLength = 40f;\" is the same as the Z-value of the scale of the Ground object (found when you select the Ground in Hierarchy and look in the Inspector at the  Transform component.",
+    code: `    public GameObject[] terrainPrefabs; // Assign in inspector
+    public int poolSize = 5;
+    public float segmentLength = 40f;
+    public Transform player;
+    private Queue<GameObject> terrainPool = new Queue<GameObject>();
+    private float nextSpawnZ = 0f;`
+   },
+   {
+    text: "Add this code to the TerrainManager script directly above void Start().",
+    code: `               // Spawn initial segments
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(terrainPrefabs[Random.Range(0, terrainPrefabs.Length)], 
+                                         Vector3.forward * nextSpawnZ, Quaternion.identity);
+            terrainPool.Enqueue(obj);
+            nextSpawnZ += segmentLength;
+        }`
+   },
+   {
+    text: "Add this code to the TerrainManager script inside \"void Update()\"",
+    code: `                // If the player has passed the first segment, recycle it
+        GameObject firstSegment = terrainPool.Peek();
+        if (player.position.z - firstSegment.transform.position.z > segmentLength)
+        {
+            // Move segment to the end
+            firstSegment = terrainPool.Dequeue();
+            firstSegment.transform.position = Vector3.forward * nextSpawnZ;
+            terrainPool.Enqueue(firstSegment);
+            nextSpawnZ += segmentLength;
+        } `
+   },
+   {
+    text: "In the Unity window, select the TerrainManager object in the Hierarchy. In the Inspector tab, click \"Add Component\" and add the \"TerrainManager\" script."
+   },
+   {
+    text: "In the Inspector tab, assign the Prefabs you created earlier to the \"Terrain Prefabs\" field of the TerrainManager script.",
+   },
+   {
+    text: "In the Inspector tab, assign the Player object to the \"Player\" field of the TerrainManager script."
    }
+
+
+
  ];
  
  export default function TerrainRecyclingPage() {
@@ -26,43 +81,25 @@
      </main>
    );
  }
-/*    <h2 id="recycling">Intro to Terrain Recycling</h2>
-    <p>Now we will make it so the world is ACTUALLY endless.</p>
-    <video controls width="640" style="display:block; margin: 2em 0;">
-        <source src="Endless-Runner-Tutorials/3TerrainRecycling.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-    <ol>
-        <li><label><input type="checkbox">Create a new folder in the Assets folder called "Prefabs".</label></li>
-        <li><label><input type="checkbox">Drag and drop your Ground object from the Hierarchy into the Prefabs folder. This creates a reusable template that the scripts we're about to make will need.</label></li>
-<li><label><input type="checkbox">Create a new script in the Scripts folder called "TerrainManager.cs".</label></li>
+/*
+    <ol>  
+        <li><label><input type="checkbox"></label></li>
+        <li><label><input type="checkbox"></label></li>
+<li><label><input type="checkbox"></label></li>
     <div style="position: relative;">
     <button onclick="copyCode(this)" style="position: absolute; top: 0; right: 0; z-index: 2;">Copy</button><pre><code class="language-csharp">
-    using System.Collections.Generic;</code></pre></div>
-    <li><label><input type="checkbox">Add this code to the TerrainManager script directly above <code>void Start()</code>. Make sure the number in <code>public float segmentLength = 40f;</code> is the same as the Z scale of the Ground object (and make sure it has the 'f' at the end).</label></li>
+    </code></pre></div>
+    <li><label><input type="checkbox">A</label></li>
     <div style="position: relative;">
     <button onclick="copyCode(this)" style="position: absolute; top: 0; right: 0; z-index: 2;">Copy</button><pre><code class="language-csharp">
-    public GameObject[] terrainPrefabs; // Assign in inspector
-    public int poolSize = 5;
-    public float segmentLength = 40f;
-    public Transform player;
 
-    private Queue<GameObject> terrainPool = new Queue<GameObject>();
-    private float nextSpawnZ = 0f;
     </code></pre></div>
     <li><label><input type="checkbox">Add this code to the TerrainManager script inside <code>void Start()</code></label></li>
     <div style="position: relative;">
     <button onclick="copyCode(this)" style="position: absolute; top: 0; right: 0; z-index: 2;">Copy</button><pre><code class="language-csharp">
-               // Spawn initial segments
-        for (int i = 0; i < poolSize; i++)
-        {
-            GameObject obj = Instantiate(terrainPrefabs[Random.Range(0, terrainPrefabs.Length)], 
-                                         Vector3.forward * nextSpawnZ, Quaternion.identity);
-            terrainPool.Enqueue(obj);
-            nextSpawnZ += segmentLength;
-        }
+
     </code></pre></div>
-        <li><label><input type="checkbox">Add this code to the TerrainManager script inside <code>void Update()</code></label></li>
+        <li><label><input type="checkbox"></label></li>
     <div style="position: relative;">
     <button onclick="copyCode(this)" style="position: absolute; top: 0; right: 0; z-index: 2;">Copy</button><pre><code class="language-csharp">
                 // If the player has passed the first segment, recycle it
@@ -76,7 +113,7 @@
             nextSpawnZ += segmentLength;
         } 
         </code></pre></div>
-        <li><label><input type="checkbox">In the Unity window, select the TerrainManager object in the Hierarchy. In the Inspector tab, click "Add Component" and add the "TerrainManager" script.</label></li>
-        <li><label><input type="checkbox">In the Inspector tab, assign the Prefabs you created earlier to the "Terrain Prefabs" field of the TerrainManager script.</label></li>
-        <li><label><input type="checkbox">In the Inspector tab, assign the Player object to the "Player" field of the TerrainManager script.</label></li>
+        <li><label><input type="checkbox"></label></li>
+        <li><label><input type="checkbox"></label></li>
+        <li><label><input type="checkbox"></label></li>
 </ol>*/
